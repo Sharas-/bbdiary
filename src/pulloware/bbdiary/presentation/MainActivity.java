bc08f3f9a070d933529c43fda297c9b8164e128d
sharas <sharas@sharas.com>
2015-06-12 16:58:12 +0300

    added some style to timer

--- src/pulloware/bbdiary/presentation/MainActivity.java
+++ src/pulloware/bbdiary/presentation/MainActivity.java
@@ -5,11 +5,9 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.SystemClock;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Chronometer;
 import android.widget.TextView;
-import android.widget.ViewFlipper;
 import pulloware.bbdiary.R;
 
 import java.io.File;
@@ -22,8 +20,6 @@ public class MainActivity extends Activity
      */
     private boolean exercising = false;
     TextView txt;
-    private ViewFlipper viewFlipper;
-    private float lastX;
 
 
     public void onCreate(Bundle savedInstanceState)
@@ -31,7 +27,6 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         txt = (TextView) findViewById(R.id.txtView);
-        viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
     }
 
     public void onClick(View v) throws Exception
@@ -90,54 +85,6 @@ public class MainActivity extends Activity
             c.start();
             exercising = true;
         }
-    }
-
-    // Method to handle touch event like left to right swap and right to left swap
-    public boolean onTouchEvent(MotionEvent touchevent)
-    {
-        switch (touchevent.getAction())
-        {
-            // when user first touches the screen to swap
-            case MotionEvent.ACTION_DOWN:
-            {
-                lastX = touchevent.getX();
-                break;
-            }
-            case MotionEvent.ACTION_UP:
-            {
-                float currentX = touchevent.getX();
-
-                // if left to right swipe on screen
-                if (lastX < currentX)
-                {
-                    // If no more View/Child to flip
-                    if (viewFlipper.getDisplayedChild() == 0)
-                        break;
-
-                    // set the required Animation type to ViewFlipper
-                    // The Next screen will come in form Left and current Screen will go OUT from Right
-                    viewFlipper.setInAnimation(this, R.anim.in_from_left);
-                    viewFlipper.setOutAnimation(this, R.anim.out_to_right);
-                    // Show the next Screen
-                    viewFlipper.showNext();
-                }
-
-                // if right to left swipe on screen
-                if (lastX > currentX)
-                {
-                    if (viewFlipper.getDisplayedChild() == 1)
-                        break;
-                    // set the required Animation type to ViewFlipper
-                    // The Next screen will come in form Right and current Screen will go OUT from Left
-                    viewFlipper.setInAnimation(this, R.anim.in_from_right);
-                    viewFlipper.setOutAnimation(this, R.anim.out_to_left);
-                    // Show The Previous Screen
-                    viewFlipper.showPrevious();
-                }
-                break;
-            }
-        }
-        return false;
     }
 }
 
